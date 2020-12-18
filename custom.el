fdd4e5465b35a0599c0bdaee90e6f437d64f5d47
Jewel James <flakyhermit@protonmail.ch>
2020-12-18 05:45:58 +0530

    Auto-commit: Fri Dec 18 05:45:58 AM IST 2020

--- custom.el
+++ custom.el
@@ -4,7 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((org-refile-targets)
+   '((org-refile-targets
+      (buffer-file-name :maxlevel . 2))
+     (org-log-into-drawer . t)
+     (org-refile-targets)
      (eval progn
            (olivetti-mode 1)
            (flyspell-mode 1))
@@ -14,5 +17,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(markdown-header-face-1 ((t (:height 140))))
+ '(markdown-header-face-1 ((t (:height 150 :inherit markdown-header-face))))
  '(org-document-title ((t (:height 150)))))
