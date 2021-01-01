2c11b2d51c42f3850fe62bb8956dc6e434630000
Jewel James <flakyhermit@protonmail.ch>
2021-01-01 21:33:29 +0530

    Custom variables

--- custom.el
+++ custom.el
@@ -3,19 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   '((eval progn
-           (olivetti-mode 1)
-           (markdown-toggle-markup-hiding t)
-           (flyspell-mode 1))
-     (org-refile-targets
-      (buffer-file-name :maxlevel . 2))
-     (org-log-into-drawer . t)
-     (eval progn
-           (olivetti-mode 1)
-           (flyspell-mode 1))
-     (org-refile-targets)
-     (org-log-done . time))))
+ '(safe-local-variable-values '((org-refile-targets) (org-log-done . time))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
