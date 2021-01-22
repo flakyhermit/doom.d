83e3110d80620b61cdef2f326016c2e7c0ccdaa7
Jewel James <flakyhermit@protonmail.ch>
2021-01-22 11:38:59 +0530

    Added GCPP stuff

--- me.el
+++ me.el
@@ -9,3 +9,10 @@
   (markdown-mode)
   (goto-char (point-max)))
 (global-set-key (kbd "C-c j s") #'my/journal-daily)
+
+(setq gcpp-file "~/Dropbox/Notes/org/knowledgebase/references/20210114174332-gcpp_takshashila.org")
+(after! org
+  (push '("g" "GCPP Takshashila") org-capture-templates)
+  (push '("gt" "Todo" entry
+          (file+headline gcpp-file "Things to-do")
+          "* TODO %?\n") org-capture-templates))
