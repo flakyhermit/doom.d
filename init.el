33a033a1271e17ee5e4fd5c520b84ab2ea4ac7c8
Jewel James <flakyhermit@protonmail.ch>
2021-01-22 11:38:33 +0530

    Added org-noter
    And misc custom.el changes

--- init.el
+++ init.el
@@ -144,6 +144,7 @@
        (org                ; organize your plain life in plain text
         +roam
         +dragndrop
+        +noter
         +pretty)
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
