de437d950bd95325ac7669108b5df734928fda81
Jewel James <flakyhermit@protonmail.ch>
2021-05-26 14:15:56 +0530

    Updated custom

--- custom.el
+++ custom.el
@@ -3,8 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '("~/Dropbox/Notes/org/knowledgebase/references/20210114174332-gcpp_takshashila.org" "/home/monk/Dropbox/Notes/org/archlinux.org" "/home/monk/Dropbox/Notes/org/bike.org" "/home/monk/Dropbox/Notes/org/blog-post-ideas.org" "/home/monk/Dropbox/Notes/org/contacts.org" "/home/monk/Dropbox/Notes/org/cv.org" "/home/monk/Dropbox/Notes/org/devices.org" "/home/monk/Dropbox/Notes/org/diabetes.org" "/home/monk/Dropbox/Notes/org/doom.org" "/home/monk/Dropbox/Notes/org/emacs-article.org" "/home/monk/Dropbox/Notes/org/emacs.org" "/home/monk/Dropbox/Notes/org/events.org" "/home/monk/Dropbox/Notes/org/facts.org" "/home/monk/Dropbox/Notes/org/finances.org" "/home/monk/Dropbox/Notes/org/gtd.org" "/home/monk/Dropbox/Notes/org/higher-studies.org" "/home/monk/Dropbox/Notes/org/hindi.org" "/home/monk/Dropbox/Notes/org/idioms.org" "/home/monk/Dropbox/Notes/org/inbox.org" "/home/monk/Dropbox/Notes/org/income-generation-ideas.org" "/home/monk/Dropbox/Notes/org/key-issues.org" "/home/monk/Dropbox/Notes/org/kindle-support.org" "/home/monk/Dropbox/Notes/org/latex-test.org" "/home/monk/Dropbox/Notes/org/listen.org" "/home/monk/Dropbox/Notes/org/long-term-goals.org" "/home/monk/Dropbox/Notes/org/minecraft.org" "/home/monk/Dropbox/Notes/org/observations.org" "/home/monk/Dropbox/Notes/org/podcasts.org" "/home/monk/Dropbox/Notes/org/projects.org" "/home/monk/Dropbox/Notes/org/purchases.org" "/home/monk/Dropbox/Notes/org/pvc-diy.org" "/home/monk/Dropbox/Notes/org/quotes.org" "/home/monk/Dropbox/Notes/org/resources.org" "/home/monk/Dropbox/Notes/org/self-improvement.org" "/home/monk/Dropbox/Notes/org/thoughts.org" "/home/monk/Dropbox/Notes/org/typing-test.org" "/home/monk/Dropbox/Notes/org/vocabulary.org" "/home/monk/Dropbox/Notes/org/watch.org"))
+ '(custom-safe-themes
+   '("711efe8b1233f2cf52f338fd7f15ce11c836d0b6240a18fffffc2cbd5bfe61b0" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" default))
  '(safe-local-variable-values
    '((org-latex-classes
       ("article" "\\RequirePackage{fix-cm}
@@ -99,11 +99,6 @@
        ("\\section{%s}" . "\\section*{%s}")
        ("\\subsection{%s}" . "\\subsection*{%s}")
        ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
-     (org-log-into-drawer . t)
-     (org-refile-targets)
-     (eval progn
-           (flyspell-mode 1)
-           (markdown-toggle-markup-hiding t))
      (org-log-done . time))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
