f80c503039c7a01d333454a2819fffb336fe4d64
Jewel James <flakyhermit@protonmail.ch>
2021-10-11 10:11:03 +0530

    Added/removed modules

--- init.el
+++ init.el
@@ -27,10 +27,10 @@
        :ui
        ;; deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
-       ;; doom-dashboard    ; a nifty splash screen for Emacs
-       ;; doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       doom-dashboard    ; a nifty splash screen for emacs
+       ;; doom-quit         ; doom quit-message prompts when you quit emacs
        ;;fill-column       ; a `fill-column' indicator
-       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       hl-todo           ; highlight todo/fixme/note/deprecated/hack/review
        hydra
        ;;indent-guides     ; highlighted indent columns
        ;;minimap           ; show a map of the code on the side
@@ -48,7 +48,7 @@
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        (window-select +numbers)     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
-       ;;zen               ; distraction-free coding or writing
+       zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
@@ -80,7 +80,7 @@
        :checkers
        syntax              ; tasing you for every semicolon you forget
        (spell +flyspell)            ; tasing you for misspelling mispelling
-       ;;grammar           ; tasing grammar mistake every you make
+       grammar           ; tasing grammar mistakevery you make
 
        :tools
        ;;ansible
